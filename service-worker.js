24fefd291ece4bb03bf3a8ed4d2e05b0cec73834
Alex Page <alex@alexpage.com.au>
2019-04-21 17:59:12 +0000

    Merge pull request #24 from alex-page/collections

--- service-worker.js
+++ service-worker.js
@@ -29,11 +29,23 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "bd936d88c2668bdf6d88da34cc2c0903"
+    "revision": "97bfe664e8deb82bd4d1c84b84ac87d7"
+  },
+  {
+    "url": "assets/blog/actions.jpg",
+    "revision": "bd92975eae17f4058c3ee93f8f6ea165"
+  },
+  {
+    "url": "assets/blog/gh-actions.jpg",
+    "revision": "668179f9dce00dcfe97ee0372d8a3cfc"
+  },
+  {
+    "url": "assets/blog/workflow.jpg",
+    "revision": "83b9342473acd69eeb89e14dc8b4be23"
   },
   {
     "url": "assets/favicons/alex-page.jpg",
-    "revision": "ad5fe5c24627624282a6cd649c026e5e"
+    "revision": "3a6795ab492eed9afdd31f60905749cb"
   },
   {
     "url": "assets/favicons/apple-touch-icon.png",
@@ -68,36 +80,80 @@ self.__precacheManifest = [
     "revision": "bd2022f41a6e2aa0028836a508aa906e"
   },
   {
-    "url": "assets/img/alex-page.jpg",
-    "revision": "5f10cdd6e12f1f68e3f66d1c46d4e257"
+    "url": "assets/icons/download.svg",
+    "revision": "570ee436fd7854a48fe3dca8db3690ee"
+  },
+  {
+    "url": "assets/icons/github.svg",
+    "revision": "312721152592a190b4bc1de83a57daf9"
+  },
+  {
+    "url": "assets/icons/mail.svg",
+    "revision": "733b40cb4c31a8602d8d143c58ff703e"
+  },
+  {
+    "url": "assets/icons/star.svg",
+    "revision": "31e079da8dc8a8225253592e5e596e9d"
+  },
+  {
+    "url": "assets/icons/twitter.svg",
+    "revision": "b4d65fd0a18509864065499fd3d7e08d"
+  },
+  {
+    "url": "assets/img/alex-page.png",
+    "revision": "597889adb823c5026fd89a4d212252bd"
   },
   {
     "url": "assets/img/logo.svg",
     "revision": "dad33d34ebd308c6417893d14557284a"
   },
   {
+    "url": "blog/github-actions-npm/index.html",
+    "revision": "251a937a8c95a879834b2d8fb2295479"
+  },
+  {
     "url": "blog/index.html",
-    "revision": "93166438d94b874fb7b657b1822ac1e2"
+    "revision": "9d953f996f05aab94235f790cc5e6526"
   },
   {
     "url": "index.html",
-    "revision": "9f9a50207f03ad4238321069722cc735"
+    "revision": "56b4afb67aaebf4ecfb8ba952997e6ad"
   },
   {
     "url": "projects/a11ycolor/index.html",
-    "revision": "344eae6321e413a3e8b979c29ef7110b"
+    "revision": "3223a496b6266eb3c810b569c2a8d1f8"
+  },
+  {
+    "url": "projects/auds/index.html",
+    "revision": "67a28a433724c5f1151fe94c897961da"
+  },
+  {
+    "url": "projects/chameleon/index.html",
+    "revision": "6a85183a886ed9aa1dc070509b6890d6"
+  },
+  {
+    "url": "projects/furnace/index.html",
+    "revision": "67a4897b21d7737a9fcc6b1bf261303b"
   },
   {
     "url": "projects/harmonograph/index.html",
-    "revision": "f125f90c13c00a6139e69ea4e5a4e700"
+    "revision": "1a00713a266077cc684bcfd526342f27"
   },
   {
     "url": "projects/index.html",
-    "revision": "8cd01df99e81d6ede0409e76a08940a8"
+    "revision": "00e29095074ccc63be0ad12422fda9b9"
+  },
+  {
+    "url": "talks/hard-work-make-simple/index.html",
+    "revision": "4981c483f5f697bd5efbcc5b4f671878"
   },
   {
     "url": "talks/index.html",
-    "revision": "fe37dc15564297064ae7409b8e75a883"
+    "revision": "da9a92c6bbbc42870a828519169996e6"
+  },
+  {
+    "url": "talks/names-culture/index.html",
+    "revision": "c0fbfb23f30550f7b326bd2e495081ae"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
